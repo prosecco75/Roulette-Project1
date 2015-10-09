a7791358262bd328ddab2f9df2944d35fa0d2a5f
prosecco75 <pygmalion1999@gmail.com>
2015-10-09 17:26:14 +0100

    HTMLCSSJS

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function(){
+  var $lis = $('.board li');
+  $lis.on("click", function(){
+    $(this).find("span").toggleClass("played");
+  })
+})
+
+// var guess = []
+// Push into guess the value of the clicked buttons
+// and decrease money :)
+
+// var number = Math.floor(Math.random() * 36);
+// if guess.indexOf(number) !== -1 
+// Then lose all of your chips
+//
+// else then you win 36 chips - chips played
