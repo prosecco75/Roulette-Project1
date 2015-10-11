fb1c1ad5989be8b49e3d970884ec41c45b724544
prosecco75 <pygmalion1999@gmail.com>
2015-10-11 17:42:46 +0100

    last

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var chosenSquares = [];
 var bet = 0;
 var pot = 50;
 var win = 0;
-var rotation = 0;
+//var rotation = 0;
 var wheelNumbers = [0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26]
 
 function play(){
@@ -37,23 +37,29 @@ function clearBoard(){
 }
 
 function spinWheel(){
-  var wheel = $(".wheel-image")[0];
-  wheel.removeAttribute('style');
-  var deg = 500 + Math.round(Math.random() * 500);
-  rotation += deg;
-  var css = '-webkit-transform: rotate(' + deg + 'deg);';
-  wheel.setAttribute('style', css);
+  // var wheel = $(".wheel-image")[0];
+  // wheel.removeAttribute('style');
+  // var deg = 500 + Math.round(Math.random() * 500);
+  // rotation += deg;
+  // var css = '-webkit-transform: rotate(' + deg + 'deg);';
+  // wheel.setAttribute('style', css);
 }
 
 function spin(){
   var chosenNumber = Math.floor(Math.random()*36)
-  $('.draw').text(chosenNumber);
+  $('.wheel').text(chosenNumber);
   spinWheel();
-  var deg = rotation //wheelNumbers.indexOf(chosenNumber)+1 * 10 + rotation;
-  console.log(rotation)
-  var bc = $('.ball-container')[0];
-  var css = '-webkit-transform: rotate(' + deg + 'deg);';
-  bc.setAttribute('style', css);
+  // var deg = rotation //wheelNumbers.indexOf(chosenNumber)+1 * 10 + rotation;
+  // console.log(rotation)
+  // var bc = $('.ball-container')[0];
+  // var css = '-webkit-transform: rotate(' + deg + 'deg);';
+  // bc.setAttribute('style', css);
+
+  // var counter = 0;
+  // setInterval(function(){
+  //   var num = wheelNumbers[Math.floor(Math.random()*wheelNumbers.length)]
+  //   $('.wheel').text(chosenNumber);
+  // }, 100)
 
   // Check if the chosen number appears in the chosenSquares
   if (chosenSquares.indexOf(chosenNumber) !== -1) {
