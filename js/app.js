fc7304f01c06792e43fc99cd7d655229de5e040c
prosecco75 <pygmalion1999@gmail.com>
2015-10-11 04:44:53 +0100

    js

--- js/app.js
+++ js/app.js
@@ -18,3 +18,45 @@ $(function(){
 // Clear the board
 // Select all of the spans and then remove the class .played
 // hint -> removeClass
+
+// Create a board to play on in html
+//   - 9 squares
+//   - similar/same class
+
+// Create events to link the squares to click
+//   - when they are clicked
+//     - 1. save which square was clicked
+//     - 2. change the color of the square
+
+// Check whether the game is over?
+//  - squares are full
+//  - when there is a winner
+
+// If there is no winner, and the game is still running you need to switch turns
+
+window.onload = function(){
+  var guess = []
+  guess.push.value(clickedIndex);
+ for (i=10,000; i<1 ; i--)
+  
+  // Create a variable to count the total number of games
+  var totalNumberGames = 0;
+
+  // Create a variable that stores all of the winning numbers
+  var winningNumbers = [
+                        BLACK numbers [0,1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36], 
+                        RED numbers   [2,4,6,8.10,11,13,15,17,20,22,24,26,28,29,31,33,35]
+                              ];
+                             
+  for (var w = 0; w < winningNumbers.length; w++) {
+
+  // Grab all of the li's on the page and save in a variable of squares 
+  var squares = document.getElementsByTagName("li");
+
+  // Loop through the squares and setup a "click" event listener
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", function(){
+
+      // When a square is clicked, we find out which index it has in the square array
+      var squaresArray = Array.prototype.slice.call(squares);
+      var clickedIndex = squaresArray.indexOf(this);
