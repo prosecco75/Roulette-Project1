364f3aa2b47057b775764f7321ad7e254bedfbf7
prosecco75 <pygmalion1999@gmail.com>
2015-10-13 18:21:01 +0100

    pushing to heroku

--- js/app.js
+++ js/app.js
@@ -5,13 +5,20 @@ var chosenSquares = [];
 var bet = 0;
 var pot = 50;
 var win = 0;
-//var rotation = 0;
-var wheelNumbers = [0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26]
+
+var wheelNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 
 function play(){
   $('.board li').on("click", chooseSquare);
   $('.spin').on("click", spin);
 }
+
+setTimeout(function(){
+  $(".wheel").prepend(
+    '<p class="animated flip">Choose your squares!<p>'
+    )
+}, 1500);
+
 
 function chooseSquare(){
   var $clickedSquare = $(this);
@@ -36,60 +43,20 @@ function clearBoard(){
   $('.played').removeClass("played");
 }
 
-function spinWheel(){
-  // var wheel = $(".wheel-image")[0];
-  // wheel.removeAttribute('style');
-  // var deg = 500 + Math.round(Math.random() * 500);
-  // rotation += deg;
-  // var css = '-webkit-transform: rotate(' + deg + 'deg);';
-  // wheel.setAttribute('style', css);
-}
-
 function spin(){
   var chosenNumber = Math.floor(Math.random()*36)
   $('.wheel').text(chosenNumber);
-  spinWheel();
-  // var deg = rotation //wheelNumbers.indexOf(chosenNumber)+1 * 10 + rotation;
-  // console.log(rotation)
-  // var bc = $('.ball-container')[0];
-  // var css = '-webkit-transform: rotate(' + deg + 'deg);';
-  // bc.setAttribute('style', css);
-
-  // var counter = 0;
-  // setInterval(function(){
-  //   var num = wheelNumbers[Math.floor(Math.random()*wheelNumbers.length)]
-  //   $('.wheel').text(chosenNumber);
-  // }, 100)
+  //spinWheel();
 
   // Check if the chosen number appears in the chosenSquares
   if (chosenSquares.indexOf(chosenNumber) !== -1) {
-    console.log("Win");
+    $('.wheel').text($('.wheel').text() + "  " + '"Congratualions, you win!!"')
+
     changeBets('.pot', 36);
     resetBet('.bet');
   } else {
-    console.log("Lose");
+    $('.wheel').text($('.wheel').text() + "  " + '"You have lost, try again!!"')
     resetBet('.bet');
   }
   clearBoard();
 }
-
-function Counter(){
-  var button     = document.getElementById('down');
-  var button     = document.getElementById('up');
-  this.player1Score      = 0;
-  this.player2Score      = 0;
-
-
-  // function choseNnumber(a,b,c){
-  //   if (a + b + c === 50) {
-  //     console.log ("You have won!");
-  //   else {
-  //     return ("You have lost");
-  //   }
-
-
-function clearTimeout() {
-  alert("")
-}
-    
-var timeoutID = window.setTimeout(code, [delay]);
